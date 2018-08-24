bb090fb6d5846f6a19bb02c087d51d4ae5997baf
William Kimball <wkkimball043@gmail.com>
2018-08-23 19:57:06 -0500

    moved API keys to gitignore

--- src/apiItems/APIKeys.js
+++ src/apiItems/APIKeys.js
@@ -1,15 +1,15 @@
 class apiKeys {
 
     static UnsplashKey = () => {
-        return "Put Unsplash API Key Here"
+        return "38b469e4ae6b45d6f859a5ce65ad4a6a0b06fc792cab0d16cc7bf052a396384c"
     }
 
     static TravelPayoutKey = () => {
-        return "Put TravelPayouts API key Here"
+        return "7fe8a6850404e8611035f004e2a6bc3f"
     }
 
     static airportSearch = () => {
-        return "Put AirportSearch API Key Here"
+        return "3d57d73c4b"
     }
 
 }
