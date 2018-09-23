5c53f9cbdf3a74f9403bb068bbe0f6ba60cd27cb
William Kimball <wkkimball043@gmail.com>
2018-09-23 17:55:20 -0500

    worked to refactor landing page

--- src/Landing.js
+++ src/Landing.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
-import { Card, CardContent, Title, Content, Button, Image } from "bloomer";
+import {
+  Card,
+  CardContent,
+  Title,
+  Content,
+  Button,
+  Image,
+  Columns,
+  Column,
+  Tile,
+  Box,
+  Container
+} from "bloomer";
 import LoginForm from "./loginForm";
 import RegisterForm from "./register";
+import apiKeys from "./apiItems/APIKeys";
 
 export default class Login extends Component {
   state = {
@@ -39,37 +52,119 @@ export default class Login extends Component {
     }
   };
 
+  getImage = async destination => {
+    var s = destination;
+    var n = s.indexOf(",");
+    s = s.substring(0, n != -1 ? n : s.length);
+    // console.log(s);
+
+    const json = await fetch(
+      `https://api.unsplash.com/photos/random/?query=travel&client_id=${apiKeys.UnsplashKey()}`
+    )
+      .then(response => response.json())
+      .then(image => {
+        if (image.urls) {
+          this.setState({
+            image: image,
+            imagePic: image.urls.regular,
+            imageUser: image.user.name,
+            imageUserLink: image.user.links.html,
+            imageLink: image.links.html,
+            imageAlt: image.description
+          });
+        }
+      });
+  };
+
+  componentDidMount() {
+    this.getImage;
+  }
+
   render() {
     return (
-      <Card hasTextAlign="centered">
-        <Title>Welcome to DartBoard!</Title>
-        <CardContent>
-          
-          <Content>
-            <div>
-              <Button
-                isColor="info"
-                id="linkToLoginForm"
-                onClick={this.loginModal}
-              >
-                Login
-              </Button>
-            </div>
-              <Image id="loginLogo" src={require('././DartBoardRed.png')}/>
-            <div>
-              <Button
-                isColor="info"
-                id="linkToRegisterForm"
-                onClick={this.registerModal}
-              >
-                Register
-              </Button>
-            </div>
-          </Content>
-        </CardContent>
-        {this.state.LoginModal}
-        {this.state.RegisterModal}
-      </Card>
+      <Column isVCentered="true">
+        <Tile isAncestor>
+          <Tile isParent>
+            <Tile
+              isChild
+              render={props => (
+                <Box {...props}>
+                  <Title hasTextAlign="center">Welcome to DartBoard!</Title>
+                  <p>
+                    DartBoard is a travel planning app that users can utilize as
+                    a central repository for information related to their travel
+                    plans.
+                  </p>
+                  <p>To get started, Login or Register for a new account.</p>
+                </Box>
+              )}
+            />
+            <Tile isSize={4} isVertical isParent>
+              <Tile
+                isChild
+                render={props => (
+                  <Box {...props}>
+                    <Title>Login</Title>
+                    <Button
+                      isColor="info"
+                      id="linkToLoginForm"
+                      onClick={this.loginModal}
+                    >
+                      Login
+                    </Button>
+                  </Box>
+                )}
+              />
+              <Tile
+                isChild
+                render={props => (
+                  <Box {...props}>
+                    <Title>Register</Title>
+                  {/* <Image isPulled="right" id="loginLogo" isSize="96x96" src={require('././DartBoardRed.png')}/> */}
+                 <Button
+                   isColor="info"
+                   id="linkToRegisterForm"
+                   onClick={this.registerModal}
+                 >
+                   Register
+                 </Button>
+                  </Box>
+                )}
+              />
+            </Tile>
+          </Tile>
+        </Tile>
+               {this.state.LoginModal}
+             {this.state.RegisterModal}
+      </Column>
+      // <Columns>
+      //   <Column isVCentered="true">
+      //     <Title hasTextAlign="center">Welcome to DartBoard!</Title>
+      //     <CardContent hasTextAlign="right">
+      //       <Content>
+      //         <div>
+      //           <Button
+      //             isColor="info"
+      //             id="linkToLoginForm"
+      //             onClick={this.loginModal}
+      //           >
+      //             Login
+      //           </Button>
+      //           {/* <Image isPulled="right" id="loginLogo" isSize="96x96" src={require('././DartBoardRed.png')}/> */}
+      //           <Button
+      //             isColor="info"
+      //             id="linkToRegisterForm"
+      //             onClick={this.registerModal}
+      //           >
+      //             Register
+      //           </Button>
+      //         </div>
+      //       </Content>
+      //       {this.state.LoginModal}
+      //       {this.state.RegisterModal}
+      //     </CardContent>
+      //   </Column>
+      // </Columns>
     );
   }
 }
